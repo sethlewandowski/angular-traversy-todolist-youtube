9553642a826ba78ef25c5eb92ab9742c8a2948ef
Seth Lewandowski <sethlew@protonmail.com>
2021-04-01 15:34:41 -0700

    ! vs ? when initializing props

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './component/todos/todos.component';
 import { TodoItemComponent } from './component/todo-item/todo-item.component';
 import { HeaderComponent } from './component/layout/header/header.component';
+import { AddTodoComponent } from './component/add-todo/add-todo.component';
 
 @NgModule({
-  declarations: [AppComponent, TodosComponent, TodoItemComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    HeaderComponent,
+    AddTodoComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/component/add-todo/add-todo.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.css'],
+})
+export class AddTodoComponent implements OnInit {
+  title?: string; // strict class initialization requres !
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
